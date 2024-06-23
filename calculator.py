537e67cc2c4caf18b4832144b4e21bcdd50e411c
DaydreamingDragon <jsperera2007@gmail.com>
2024-06-23 19:24:15 -0400

    added input for first number

--- calculator.py
+++ calculator.py
@@ -1,1 +1,14 @@
 import math
+
+input1 = input("Enter first number: (type pi for pi): ")
+if input1 == "pi":
+    n1 = math.pi
+else:
+    try:
+        n1 = int(input1)
+    except ValueError:
+        try:
+            n1 = float(input1)
+        except ValueError:
+            print("Please enter a valid decimal number or pi!")
+print(f"{n1}")
