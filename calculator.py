bdd89253a9de64b5b7b99f90a15dea93317ef5ef
DaydreamingDragon <jsperera2007@gmail.com>
2024-06-23 19:31:56 -0400

    added e and tau for input 1.  Added input for calculator function.

--- calculator.py
+++ calculator.py
@@ -1,8 +1,12 @@
 import math
 
-input1 = input("Enter first number: (type pi for pi): ")
+input1 = input("Enter first number: (type pi for pi, tau for tau, or e for euler's number): ")
 if input1 == "pi":
     n1 = math.pi
+elif input1 == "e":
+    n1 = math.e
+elif input1 == "tau":
+    n1 = math.tau
 else:
     try:
         n1 = int(input1)
@@ -11,4 +15,10 @@ else:
             n1 = float(input1)
         except ValueError:
             print("Please enter a valid decimal number or pi!")
-print(f"{n1}")
+            quit()
+
+function = input("Enter function: add, subtract, multiply, divide, exponent, mod: ")
+if function != "add" and function != "subtract" and function != "multiply" and function != "divide" and function != "exponent" and function != "mod":
+    print("Please enter a function listed above!")
+    quit()
+
