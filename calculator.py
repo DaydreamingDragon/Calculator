ebfa2a6b67c6eb95706ee163a2baf700f26ac11c
DaydreamingDragon <jsperera2007@gmail.com>
2024-06-23 20:23:49 -0400

    removed code from firstNumberInput and added firstNumberInput function

--- calculator.py
+++ calculator.py
@@ -1,29 +1,12 @@
-import math
-from helper import oneNumber, twoNumbers
-
-# Get input for first number
-input1 = input("Enter first number: (type pi for pi, tau for tau, or e for euler's number): ")
-if input1 == "pi":
-    n1 = math.pi
-elif input1 == "e":
-    n1 = math.e
-elif input1 == "tau":
-    n1 = math.tau
-else:
-    try:
-        n1 = int(input1)
-    except ValueError:
-        try:
-            n1 = float(input1)
-        except ValueError:
-            print("Please enter a valid decimal number, pi, e, or tau!")
-            quit()
+from helper import oneNumber, twoNumbers, firstNumberInput
 
 # Get input for one or two number functions
 i = input("Do you want a function for one or two numbers (type one or two): ")
 if i == "one":
+    n1 = firstNumberInput()
     oneNumber(n1)
 elif i == "two":
+    n1 = firstNumberInput()
     twoNumbers(n1)
 else:
     print("Please enter the number of numbers you want to perform functions on!")
